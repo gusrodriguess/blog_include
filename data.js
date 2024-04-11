b120693cc5001254d6fd680a43c16b7dcdef66e6
unknown <gustavorodrigues_170@outlook.com>
2024-04-11 20:48:06 -0300

    Update final

--- data.js
+++ data.js
@@ -3,21 +3,21 @@ let postagens = [
         id: 1,
         imagem: "img/equipe.png",
         titulo: "Trabalho em equipe",
-        descricao: "descricao1",
+        descricao: "Tenho ótimas habilidades para trabalhar em equipe, que foram aprimoradas ao longo da minha jornada acadêmica. Minha paixão por jogos eletrônicos e esportes contribuiu bastante para o desenvolvimento dessa competência",
         visibilidade: true
     },
     {
         id: 2,
         imagem: "img/proativo.png",
         titulo: "Proatividade",
-        descricao: "descr ic ao2a aaaaaaaa aaa aaaaaaa aaaaaaaa aaaa aaaaa aaaaaa aaa aaa aaaaa aaaa aaaa aaa aa aa",
+        descricao: "Estou sempre disposto a tomar a iniciativa no desenvolvimento de projetos ou atividades",
         visibilidade: true
     },
     {   
         id: 3,
         imagem: "img/organizacao.png",
         titulo: "Organização",
-        descricao: "descricao3",
+        descricao: "As diversas tarefas ao longo da minha jornada acadêmica me ajudaram a desenvolver habilidades de organização e gestão de tempo",
         visibilidade: true
     }
 ]
